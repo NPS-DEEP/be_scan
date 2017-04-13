1fc980494b17ba49434e06489c7aef98f6332da6
Bruce Allen <bdallen@nps.edu>
2017-04-13 14:11:28 -0700

    merge functionality into artscan

--- python_bindings/artscan.py
+++ python_bindings/artscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+import os
 import be_scan
 
 # escape unprintable bytes
@@ -17,29 +18,53 @@ def escape(data):
 if __name__=="__main__":
 
     # parse filename, offset, count
-    parser = ArgumentParser(prog='artscan.py',
+    parser = ArgumentParser(prog='artscan_all.py',
              description="Scan a file for forensic artifacts.")
     parser.add_argument("filename", help="file to scan", type=str)
-    parser.add_argument("offset", help="offset in file to start", type=int)
-    parser.add_argument("count", help="number of bytes to scan", type=int)
+    parser.add_argument("-s", "--start",
+                        help="start offset in file",
+                        default=0, type=int)
+    parser.add_argument("-c", "--count",
+                        help="number of bytes to process",
+                        default=0, type=int)
+    parser.add_argument("-b", "--break_size",
+                        help="size to split processing into",
+                        default=2**27, type=int)  # 2^27 = 128MiB = 134217728
+    parser.add_argument("-v", "--verbose",
+                        help="show the input bytes",
+                        action="store_true")
     args = parser.parse_args()
 
-    # open the file for reading binary
-    with open(args.filename, mode='rb') as f:
-        f.seek(args.offset)
-        data = f.read(args.count)
-        print("data: '%s'" % escape(data))
+    offset = args.start
+    filesize = os.stat(args.filename).st_size
+    splitsize = args.break_size
 
-    # scan the data
-    scanner = be_scan.be_scan_t("email", data, len(data))
-    while True:
-        artifact = scanner.next_artifact()
-        if artifact.artifact_class == "":
-            break
-        print("artifact at %d: %s: '%s', '%s'" %(
-                 args.offset + artifact.buffer_offset,
-                 artifact.artifact_class,
-                 escape(artifact.artifact),
-                 escape(artifact.context)))
+    while (offset < filesize):
+
+        # open the file for reading binary
+        with open(args.filename, mode='rb') as f:
+            f.seek(offset)
+            data = f.read(splitsize)
+
+            # runtime status
+            print("File %s start %d count %d\n" % (args.filename, offset,
+                                                   len(data)))
+            if args.verbose:
+                print("%s\n" % escape(data))
+
+            # scan the data
+            scanner = be_scan.be_scan_t("email", data, len(data))
+            while True:
+                artifact = scanner.next_artifact()
+                if artifact.artifact_class == "":
+                    break
+                print("artifact at %d: %s: '%s', '%s'" %(
+                         offset + artifact.buffer_offset,
+                         artifact.artifact_class,
+                         escape(artifact.artifact),
+                         escape(artifact.context)))
+
+        offset += splitsize
+
     print("Done.")
 
